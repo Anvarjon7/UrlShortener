0d02bc0880de5dba5d7372d4d7fbf9626ca2e901
Win-10 <azimovanvarjon777@gmail.com>
2024-05-08 10:48:48 +0200

    init commit

---
+++ src/test/java/de/telran/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
