2c6c617f674c4b94cf95bdd9c8de2ba45c05fc4f
Win-10 <azimovanvarjon777@gmail.com>
2024-05-13 11:19:30 +0200

    fixed bag with UserRepository

--- src/main/java/de/telran/urlshortener/model/entity/user/User.java
+++ src/main/java/de/telran/urlshortener/model/entity/user/User.java
@@ -28,18 +28,18 @@ import java.util.Set;
                 attributeNodes = {@NamedAttributeNode("bindings"),@NamedAttributeNode("subscriptions")}
         )
 })
-@Table(name = "\"user\"")
+@Table(name = "user_")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @Column(name = "first_name")
-//    private String firstName;
-//
-//    @Column(name = "last_name")
-//    private String lastName;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
 
     @Column(name = "email",nullable = false,unique = true)
     private String email;
--- src/main/java/de/telran/urlshortener/repository/UserRepository.java
+++ src/main/java/de/telran/urlshortener/repository/UserRepository.java
@@ -3,17 +3,23 @@ package de.telran.urlshortener.repository;
 import de.telran.urlshortener.model.entity.user.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    @EntityGraph(value = "User.withSubscriptions", type = EntityGraph.EntityGraphType.LOAD)
+    @EntityGraph(value ="User.withSubscriptions",type =EntityGraph.EntityGraphType.LOAD)
+    @Query("select u from User u where u.id = :id")
     Optional<User> findByIdWithSubscriptions(Long id);
+
     @EntityGraph(value = "User.withUrlBindings", type = EntityGraph.EntityGraphType.LOAD)
+    @Query("select u from User u where u.id = :id")
     Optional<User> findByIdWithUrlBindings(Long id);
+
     @EntityGraph(value = "User.withUrlBindingsAndSubscriptions", type = EntityGraph.EntityGraphType.LOAD)
+    @Query("select u from User u where u.id = :id")
     Optional<User> findByIdWithUrlBindingsAndSubscriptions(Long id);
 }
