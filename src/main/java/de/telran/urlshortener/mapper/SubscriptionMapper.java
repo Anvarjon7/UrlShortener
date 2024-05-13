853127c3aba366f7a078493f671d316e690d3711
Win-10 <azimovanvarjon777@gmail.com>
2024-05-13 20:08:02 +0200

    fixed bag

--- src/main/java/de/telran/urlshortener/mapper/SubscriptionMapper.java
+++ src/main/java/de/telran/urlshortener/mapper/SubscriptionMapper.java
@@ -5,9 +5,9 @@ import de.telran.urlshortener.model.entity.subscription.Subscription;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SubscriptionMapper {
+    public class SubscriptionMapper {
 
-    public SubscriptionResponseDto toSubscriptionResponseDto(Subscription subscription) {
+        public SubscriptionResponseDto toSubscriptionResponseDto(Subscription subscription) {
         SubscriptionResponseDto subscriptionResponseDto = new SubscriptionResponseDto();
         subscriptionResponseDto.setId(subscription.getId());
         subscriptionResponseDto.setUser(subscription.getUser());
@@ -15,5 +15,5 @@ public class SubscriptionMapper {
         subscriptionResponseDto.setExpirationDate(subscription.getExpirationDate());
         subscriptionResponseDto.setCreationDate(subscription.getCreationDate());
         return subscriptionResponseDto;
-    }
+        }
 }
