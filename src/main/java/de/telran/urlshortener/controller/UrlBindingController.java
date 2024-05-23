89b139dfde12119af0fe3a3998d3819976c4699e
dzxd16 <dzxd16@gmail.com>
2024-05-23 18:19:15 +0300

    added UrlBindingController

---
+++ src/main/java/de/telran/urlshortener/controller/UrlBindingController.java
@@ -0,0 +1,53 @@
+package de.telran.urlshortener.controller;
+
+
+import de.telran.urlshortener.dto.UrlBindingCreateRequestDto;
+import de.telran.urlshortener.dto.UrlBindingResponseDto;
+import de.telran.urlshortener.service.UrlBindingService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+
+@RestController
+@RequestMapping(value = "/api/url-binding")
+@RequiredArgsConstructor
+public class UrlBindingController {
+
+    private final UrlBindingService urlBindingService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<UrlBindingResponseDto> createUrlBinding(@RequestBody @Valid UrlBindingCreateRequestDto urlBindingCreateRequestDto) {
+        UrlBindingResponseDto urlBindingResponseDto = urlBindingService.createUrlBinding(urlBindingCreateRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(urlBindingResponseDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity closeUrlBinding(@PathVariable Long id) {
+        urlBindingService.closeUrlBinding(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUrlBinding(@PathVariable Long id) {
+        urlBindingService.deleteUrlBinding(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Set<UrlBindingResponseDto>> getByUserId(@PathVariable Long id) {
+        Set<UrlBindingResponseDto> urlBinding = urlBindingService.getByUserId(id);
+        return ResponseEntity.ok(urlBinding);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UrlBindingResponseDto> getByUid(@PathVariable String uid) {
+        UrlBindingResponseDto urlBindingResponseDto = urlBindingService.getByUid(uid);
+        return new ResponseEntity<>(urlBindingResponseDto, HttpStatus.OK);
+    }
+}
