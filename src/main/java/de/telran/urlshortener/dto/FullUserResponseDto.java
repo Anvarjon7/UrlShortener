cbf589152e947c1eecef0af2c35589857a674fca
Alinanero <sandranero10@gmail.com>
2024-05-29 14:32:10 +0200

    fixed SubscriptionController, added Annotation JsonInclude

--- src/main/java/de/telran/urlshortener/dto/FullUserResponseDto.java
+++ src/main/java/de/telran/urlshortener/dto/FullUserResponseDto.java
@@ -1,5 +1,6 @@
 package de.telran.urlshortener.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import de.telran.urlshortener.model.entity.binding.UrlBinding;
 import de.telran.urlshortener.model.entity.subscription.Subscription;
 
@@ -7,7 +8,9 @@ import java.util.Set;
 
 public record FullUserResponseDto(
         UserResponseDto userResponseDto,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         Set<Subscription> subscriptions,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         Set<UrlBinding> bindings) {
 
 }
