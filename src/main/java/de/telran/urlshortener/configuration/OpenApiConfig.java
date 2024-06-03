c39bb7aec4f720eb1154a7e328f8df4c68dec916
dzxd16 <dzxd16@gmail.com>
2024-06-03 15:48:20 +0300

    added OpenApiConfig

---
+++ src/main/java/de/telran/urlshortener/configuration/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package de.telran.urlshortener.configuration;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+    public class OpenApiConfig {
+
+        @Bean
+        public OpenAPI openAPI() {
+            return new OpenAPI()
+                    .info(new Info().title("UrlShortenet project"))
+                    .addSecurityItem(new SecurityRequirement()
+                            .addList("Bearer Authentication"))
+                    .components(new Components()
+                            .addSecuritySchemes("Bearer Authentication",jwtSecurityScheme()));
+
+        }
+
+        private SecurityScheme jwtSecurityScheme() {
+            return new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .bearerFormat("JWT")
+                    .scheme("bearer");
+        }
+}
