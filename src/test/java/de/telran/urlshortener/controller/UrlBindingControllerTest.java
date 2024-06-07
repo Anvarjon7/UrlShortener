ca95a886e2b27e38bbb6c5d7131830d2c3e0adc4
dzxd16 <dzxd16@gmail.com>
2024-06-08 00:00:11 +0300

    changed UrlBindingControllerTest

--- src/test/java/de/telran/urlshortener/controller/UrlBindingControllerTest.java
+++ src/test/java/de/telran/urlshortener/controller/UrlBindingControllerTest.java
@@ -1,33 +1,3 @@
-//package de.telran.urlshortener.controller;
-//
-//import de.telran.urlshortener.dto.UrlBindingCreateRequestDto;
-//import de.telran.urlshortener.dto.UrlBindingResponseDto;
-//import de.telran.urlshortener.mapper.Mapper;
-//import de.telran.urlshortener.model.entity.binding.UrlBinding;
-//import de.telran.urlshortener.service.UrlBindingService;
-//import de.telran.urlshortener.service.UserService;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//
-//import java.time.LocalDate;
-//import java.util.Optional;
-//import java.util.Set;
-//
-//import static org.mockito.BDDMockito.given;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-//@WebMvcTest(controllers = UrlBindingController.class)
-//@AutoConfigureMockMvc(addFilters = false)
-//@ExtendWith(MockitoExtension.class)
-
 package de.telran.urlshortener.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -38,6 +8,7 @@ import de.telran.urlshortener.model.entity.binding.UrlBinding;
 import de.telran.urlshortener.security.JwtService;
 import de.telran.urlshortener.service.UrlBindingService;
 import de.telran.urlshortener.service.UserService;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -51,11 +22,17 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.Set;
 
+import static de.telran.urlshortener.repositoryTest.RepositoryTestData.USER1;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -77,112 +54,196 @@ class UrlBindingControllerTest {
     private Mapper<UrlBinding, UrlBindingResponseDto> mapper;
     @MockBean
     private JwtService jwtService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @Test
-    void createUrlBinding() throws Exception {
+    @SneakyThrows
+    void createTest() {
+        // Подготовка входных данных для теста
         UrlBindingCreateRequestDto requestDto = new UrlBindingCreateRequestDto("http://google.com", "bmw", LocalDate.now());
-        UrlBindingResponseDto responseDto = new UrlBindingResponseDto(1L, "http://google.com", "http://g.com", 0L, LocalDate.now(), LocalDate.now().plusDays(30));
 
-        given(urlBindingService.create(requestDto)).willReturn(new UrlBinding());
-        given(mapper.toDto(new UrlBinding())).willReturn(responseDto);
+        // Подготовка мока возвращаемого значения из сервиса
+        UrlBinding urlBinding = UrlBinding.builder()
+                .id(1L)
+                .originalUrl("http://google.com")
+                .count(0L)
+                .expirationDate(LocalDate.now().plusDays(30))
+                .creationDate(LocalDate.now())
+                .build();
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/url-bindings/create")
+        // Подготовка мока возвращаемого значения из маппера
+        UrlBindingResponseDto responseDto = new UrlBindingResponseDto(1L, "http://google.com", "http://g.com/shortened-url", 0L, LocalDate.now(), LocalDate.now().plusDays(30));
+
+        // Настройка моков
+        when(urlBindingService.create(any(UrlBindingCreateRequestDto.class))).thenReturn(urlBinding);
+        when(mapper.toDto(any(UrlBinding.class))).thenReturn(responseDto);
+
+        // Выполнение запроса и проверка статуса ответа
+        var result = mockMvc.perform(MockMvcRequestBuilders.post("/api/url-bindings/create")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"originalUrl\":\"http://google.com\"}"))
+                        .content(objectMapper.writeValueAsString(requestDto))
+                        .characterEncoding("UTF-8"))
                 .andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id").value(1L))
-                .andExpect(jsonPath("$.shortUrl").value("http://g.com/abc"))
-                .andExpect(jsonPath("$.originalUrl").value("http://google.com"))
-                .andExpect(jsonPath("$.closed").value(false));
+                .andReturn();
+
+        // Чтение тела ответа
+        String jsonResponse = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        // Десериализация ответа
+        UrlBindingResponseDto actual = objectMapper.readValue(jsonResponse, UrlBindingResponseDto.class);
+
+        // Проверка полей десериализованного ответа
+        assertNotNull(actual.id());
+        assertEquals(requestDto.originalUrl(), actual.originalUrl());
+        assertEquals(responseDto.shortUrl(), actual.shortUrl());
+
     }
 
+
     @Test
-    void closeUrlBinding() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.put("/api/url-bindings/1"))
+    @SneakyThrows
+    void closeTest() {
+
+        Long Id = 1L;
+        doNothing().when(urlBindingService).close(Id);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/url-bindings/{id}", Id)
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+
+        verify(urlBindingService).close(Id);
     }
 
     @Test
-    void deleteUrlBinding() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/url-bindings/1"))
+    @SneakyThrows
+    void deleteTest() {
+        Long Id = 1L;
+        doNothing().when(urlBindingService).delete(Id);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/url-bindings/{id}", Id)
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+
+        verify(urlBindingService).delete(Id);
     }
 
     @Test
-    void getByUserId() throws Exception {
+    @SneakyThrows
+    void getByUserId() {
         Long userId = 1L;
         Set<UrlBindingResponseDto> responseSet = Set.of(
                 new UrlBindingResponseDto(1L,
-                        "http://g.com/abc",
                         "http://google.com",
+                        "http://g.com/abc",
                         0L,
                         LocalDate.now(),
                         LocalDate.now().plusDays(30)),
                 new UrlBindingResponseDto(2L,
-                        "http://g.com/abc",
                         "http://google.com",
+                        "http://g.com/xyz",
                         0L,
                         LocalDate.now(),
-                        LocalDate.now().plusDays(30)));
+                        LocalDate.now().plusDays(30))
+        );
+
+        Set<UrlBinding> urlBindingSet = Set.of(
+                UrlBinding.builder()
+                        .id(1L)
+                        .originalUrl("http://google.com")
+                        .count(0L)
+                        .user(USER1)
+                        .expirationDate(LocalDate.now().plusDays(30))
+                        .creationDate(LocalDate.now())
+                        .build(),
+                UrlBinding.builder()
+                        .id(2L)
+                        .originalUrl("http://google.com")
+                        .count(0L)
+                        .user(USER1)
+                        .expirationDate(LocalDate.now().plusDays(30))
+                        .creationDate(LocalDate.now())
+                        .build()
+        );
 
         given(userService.getCurrentUserId()).willReturn(userId);
-        given(urlBindingService.getByUserId(userId)).willReturn(Set.of(new UrlBinding()));
-        given(mapper.toDtoSet(Set.of(new UrlBinding()))).willReturn(responseSet);
+        given(urlBindingService.getByUserId(userId)).willReturn(urlBindingSet);
+        given(mapper.toDtoSet(urlBindingSet)).willReturn(responseSet);
 
-        mockMvc.perform(get("/api/url-bindings"))
+        // Выполнение запроса и проверка статуса ответа
+        var result = mockMvc.perform(MockMvcRequestBuilders.get("/api/url-bindings")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].id").value(1L))
-                .andExpect(jsonPath("$[0].shortUrl").value("http://g.com/abc"))
-                .andExpect(jsonPath("$[0].originalUrl").value("http://google.com"))
-                .andExpect(jsonPath("$[0].closed").value(false))
-                .andExpect(jsonPath("$[1].id").value(2L))
-                .andExpect(jsonPath("$[1].shortUrl").value("http://g.com/def"))
-                .andExpect(jsonPath("$[1].originalUrl").value("http://google.com"))
-                .andExpect(jsonPath("$[1].closed").value(false));
+                .andReturn();
+
+        // Чтение тела ответа
+        String jsonResponse = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        // Десериализация ответа
+        Set<UrlBindingResponseDto> actualSet = objectMapper.readValue(jsonResponse,
+                objectMapper.getTypeFactory().constructCollectionType(Set.class, UrlBindingResponseDto.class));
+
+        // Проверка полей десериализованного ответа
+        assertEquals(responseSet.size(), actualSet.size());
+        for (UrlBindingResponseDto actual : actualSet) {
+            UrlBindingResponseDto expected = responseSet.stream()
+                    .filter(dto -> dto.id().equals(actual.id()))
+                    .findFirst()
+                    .orElseThrow();
+            assertEquals(expected.id(), actual.id());
+            assertEquals(expected.originalUrl(), actual.originalUrl());
+            assertEquals(expected.count(), actual.count());
+            assertEquals(expected.creationDate(), actual.creationDate());
+            assertEquals(expected.expirationDate(), actual.expirationDate());
+        }
     }
 
+
     @Test
-    void getByUid() throws Exception {
+    @SneakyThrows
+    void getByUid() {
         String uid = "/uid";
+        // Подготовка мока возвращаемого значения из сервиса
+        UrlBinding urlBinding = UrlBinding.builder()
+                .id(1L)
+                .originalUrl("http://google.com")
+                .count(0L)
+                .expirationDate(LocalDate.now().plusDays(30))
+                .creationDate(LocalDate.now())
+                .build();
         UrlBindingResponseDto responseDto = new UrlBindingResponseDto(1L, "http://google.com", "http://g.com", 0L, LocalDate.now(), LocalDate.now().plusDays(30));
 
-        given(urlBindingService.getByUid(uid)).willReturn(Optional.of(new UrlBinding()));
-        given(mapper.toDto(new UrlBinding())).willReturn(responseDto);
+        // Выполнение запроса и проверка статуса ответа
+        var result = mockMvc.perform(MockMvcRequestBuilders.get("/api/url-bindings")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+
+        given(urlBindingService.getByUid(uid)).willReturn(Optional.of(urlBinding));
+        given(mapper.toDto(urlBinding)).willReturn(responseDto);
 
         mockMvc.perform(get("/api/url-bindings/uid/{uid}", uid))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id").value(1L))
-                .andExpect(jsonPath("$.shortUrl").value("http://g.com/abc"))
+//                .andExpect(jsonPath("$.shortUrl").value("http://g.com/abc"))
                 .andExpect(jsonPath("$.originalUrl").value("http://google.com"))
                 .andExpect(jsonPath("$.closed").value(false));
     }
 
     @Test
-    void getByShortUrl() throws Exception {
+    @SneakyThrows
+    void getByShortUrl() {
         String shortUrl = "_shortUrl";
         UrlBindingResponseDto responseDto = new UrlBindingResponseDto(1L, "http://google.com", "_shortUrl", 0L, LocalDate.now(), LocalDate.now().plusDays(30));
         UrlBinding urlBinding = new UrlBinding();
-//        UrlBinding urlBinding = new UrlBinding().toBuilder()
-//                .id(1L)
-//                .uid("_shortUrl")
-//                .pathPrefix("")
-//                .baseUrl("")
-//                .user(USER1)
-//                .originalUrl("http://google.com")
-//                .build();
         given(urlBindingService.getByShortUrl(shortUrl, true)).willReturn(new UrlBinding());
-//        given(mapper.toDto(urlBinding)).willReturn(responseDto);
 
         ResultActions resultActions = mockMvc.perform(get("/api/url-bindings/shortUrl/{shortUrl}", shortUrl));
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$.id").value(1L))
-//                .andExpect(jsonPath("$.shortUrl").value("http://g.com"))
-//                .andExpect(jsonPath("$.originalUrl").value("http://google.com"))
-//                .andExpect(jsonPath("$.closed").value(false));
         // Получение результата
         MvcResult mvcResult = resultActions.andReturn();
 
@@ -194,7 +255,8 @@ class UrlBindingControllerTest {
     }
 
     @Test
-    void getByShortUrl1() throws Exception {
+    @SneakyThrows
+    void getByShortUrl1() {
         String shortUrl = "_shortUrl";
         UrlBindingResponseDto responseDto = new UrlBindingResponseDto(1L, "http://google.com", "_shortUrl", 0L, LocalDate.now(), LocalDate.now().plusDays(30));
         UrlBinding urlBinding = new UrlBinding();
@@ -213,7 +275,8 @@ class UrlBindingControllerTest {
 
 
     @Test
-    void testGetByShortUrl() throws Exception {
+    @SneakyThrows
+    void testGetByShortUrl()  {
         String shortUrl = "_shortUrl";
         UrlBindingResponseDto responseDto = new UrlBindingResponseDto(1L, "http://google.com", "_shortUrl", 0L, LocalDate.now(), LocalDate.now().plusDays(30));
         UrlBinding urlBinding = new UrlBinding();
@@ -248,6 +311,35 @@ class UrlBindingControllerTest {
 //        assertEquals(LocalDate.now(), resultDto.getCreatedDate());
 //        assertEquals(LocalDate.now().plusDays(30), resultDto.getExpirationDate());
 //        assertFalse(resultDto.isClosed());
+    }
+
+
+    @Test
+    @SneakyThrows
+    void testGetByShortUrl1()  {
+        String shortUrl = "_shortUrl";
+        UrlBindingResponseDto responseDto = new UrlBindingResponseDto(1L, "http://google.com", "_shortUrl", 0L, LocalDate.now(), LocalDate.now().plusDays(30));
+        UrlBinding urlBinding = new UrlBinding();
+
+        // Настройка мока для сервиса и маппера
+        given(urlBindingService.getByShortUrl(shortUrl, false)).willReturn(urlBinding);
+        given(mapper.toDto(urlBinding)).willReturn(responseDto);
+
+        // Выполнение запроса
+        ResultActions resultActions = mockMvc.perform(get("/api/url-bindings/shortUrl/{shortUrl}", shortUrl))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.shortUrl").value("_shortUrl"))
+                .andExpect(jsonPath("$.originalUrl").value("http://google.com"))
+                .andExpect(jsonPath("$.closed").value(false));
+
+        // Получение результата
+        MvcResult mvcResult = resultActions.andReturn();
+
+        // Преобразование JSON ответа в объект
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        System.out.println("JSON Response: " + jsonResponse); // Для отладки
     }
 
 
