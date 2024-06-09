ecef7c40fa2694a8bab576f65f9158507037c81b
Win-10 <azimovanvarjon777@gmail.com>
2024-06-09 17:11:36 +0200

    refactoring UrlBindingServiceImpl

--- src/main/java/de/telran/urlshortener/controller/StatisticsController.java
+++ src/main/java/de/telran/urlshortener/controller/StatisticsController.java
@@ -5,6 +5,7 @@ import de.telran.urlshortener.dto.statistics.UserStatisticsResponse;
 import de.telran.urlshortener.service.StatisticsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,22 +20,26 @@ public class StatisticsController {
 
     private final StatisticsService statisticsService;
 
+//    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{userId}")
     public ResponseEntity<UserStatisticsResponse> getByUser(@PathVariable Long userId) {
         return ResponseEntity.ok(statisticsService.getByUser(userId));
     }
 
+//    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/current")
     public ResponseEntity<UserStatisticsResponse> getByCurrentUser() {
         return ResponseEntity.ok(statisticsService.getByCurrentUser());
     }
 
+//    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/top/{top}")
     public ResponseEntity<TopBindingStatisticsResponse> getTopStatistics(@PathVariable int top) {
         TopBindingStatisticsResponse topBindingStatistics = statisticsService.getBindingTop(top);
         return ResponseEntity.ok(topBindingStatistics);
     }
 
+//    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<UserStatisticsResponse>> getAll() {
         return ResponseEntity.ok(statisticsService.getAll());
--- src/main/java/de/telran/urlshortener/dto/UserRequestDto.java
+++ src/main/java/de/telran/urlshortener/dto/UserRequestDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import de.telran.urlshortener.model.entity.user.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,10 +31,10 @@ public class UserRequestDto {
     private String email;
 
     @Size(min = 5, max = 30, message = "Password must be between 8 and 30 characters")
-//    @Pattern(
-//            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{5,30}$",
-//            message = "Password must be between 5 and 30 characters, and include at least one digit, one lowercase letter, one uppercase letter, and one special character (!@#$%^&*)"
-//    )
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{5,30}$",
+            message = "Password must be between 5 and 30 characters, and include at least one digit, one lowercase letter, one uppercase letter, and one special character (!@#$%^&*)"
+    )
     private String password;
 
     private Role role;
